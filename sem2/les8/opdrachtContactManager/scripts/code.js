2923b38136eacfc585a231b32b8bfe179dce81ae
lunabrouckmeersch <luna.brouckmeersch@student.vives.be>
2024-04-20 17:11:47 +0200

    labo8

--- sem2/les8/opdrachtContactManager/scripts/code.js
+++ sem2/les8/opdrachtContactManager/scripts/code.js
@@ -6,44 +6,53 @@ const bewaarBewerktePersoon = () => {
     console.log("Klik op de knop bewaar");
 
     // valideer alle input data en controleer of er geen errors meer zijn
-    valideer();
-    if(document.getElementsByClassName("invalid").length === 0){
-        const selectedIndex = document.getElementById("lstPersonen").selectedIndex;
-        if(selectedIndex===-1) {
-            let persoon = {
-                voornaam: document.getElementById("txtVoornaam").value,
-                familienaam: document.getElementById("txtFamilienaam").value,
-                geboorteDatum: document.getElementById("txtGeboorteDatum").value,
-                email: document.getElementById("txtEmail").value,
-                aantalKinderen: document.getElementById("txtKinderen").value
-            };
-            personen.push(persoon);
-        } else {
-            personen[selectedIndex].voornaam = document.getElementById("txtVoornaam").value;
-            personen[selectedIndex].achternaam = document.getElementById("txtFamilienaam").value;
-            personen[selectedIndex].geboorteDatum = document.getElementById("txtGeboorteDatum").value;
-            personen[selectedIndex].email = document.getElementById("txtEmail").value;
-            personen[selectedIndex].aantalKinderen = document.getElementById("txtAantalKinderen").value;
-        }
+    //valideer();
+    let persoon ={
     }
+    vulPersoonOpBasisVanUserInterface(persoon)
+    personen.push(persoon)
+    console.log(persoon)
+    voegPersoonToeAanLijstInUserInterface(persoon)
     // indien ok, bewaar de ingegeven data.
-
-        // een nieuw aangemaakte persoon voegen we toe
-        // een bestaande persoon in de lijst passen we aan
+    // een nieuw aangemaakte persoon voegen we toe
+    // een bestaande persoon in de lijst passen we aan
 
     // zorg ervoor dat de naam en voornaam ook aangepast en/of zichtbaar zijn in de lijst na updaten
 };
+const vulPersoonOpBasisVanUserInterface = (persoon) => {
 
+    persoon.Voornaam = document.getElementById("txtVoornaam").value.trim();
+    persoon.familienaam = document.getElementById("txtFamilienaam").value.trim();
+    persoon.GeboorteDatum = document.getElementById("txtGeboorteDatum").value.trim();
+    persoon.Email = document.getElementById("txtEmail").value.trim();
+    persoon.AantalKinderen = document.getElementById("txtAantalKinderen").value.trim();
+
+}
+
+const voegPersoonToeAanLijstInUserInterface = (persoon) => {
+    let lijst = document.getElementById("lstPersonen")
+    let option = document.createElement("option")
+    option.text = persoon.Voornaam + " " +  persoon.familienaam;
+    option.value = persoon.Voornaam + " " +  persoon.familienaam;
+    option.setAttribute("data-index", personen.indexOf(persoon.Voornaam))
+    lijst.appendChild(option);
+    lijst.selectedindex = personen.length - 1;
+
+}
 // Event listener (btnNieuw click)
 const bewerkNieuwePersoon = () => {
     console.log("Klik op de knop nieuw");
-
+    let lijst = document.getElementById("lstPersonen")
+    lijst.selectedindex = - 1;
+    //input[type text] gaat alle inputs selecteren die type text zijn
+    let inputelem = document.querySelectorAll('input[type=text]')
+    inputelem.forEach((elem) => {
+        elem.value = "";
+    });
     // Zet de user interface klaar om de gegevens van een nieuwe persoon in te voeren
 };
 
-const resetForm = () => {
 
-}
 // onze setup functie die de event listeners registreert
 const setup = () => {
     let btnBewaar = document.getElementById("btnBewaar");
@@ -53,6 +62,7 @@ const setup = () => {
     btnNieuw.addEventListener("click", bewerkNieuwePersoon);
 
     let lstPersonen = document.getElementById("lstPersonen");
+    lstPersonen.addEventListener("click", vulPersoonOpBasisVanUserInterface)
     // voeg een change listener toe aan lstPersonen. Bij het klikken op een option element in de lijst
     // moet de data van die persoon getoond worden in het formulier
 };
